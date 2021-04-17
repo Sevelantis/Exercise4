045da39540490f37e1033a605d29559d3042b603
Miron <oskroba.miron@gmail.com>
2021-04-17 21:16:28 +0200

    added normalize() to app.py, works for 1 nest keywords

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
+import string
 
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+
 app = Flask(__name__)
 
 
@@ -8,9 +11,22 @@ app = Flask(__name__)
 def hello():
     return 'Hello Flask from alpine-linux!'
 
-@app.route('/json', methods=["POST"])
-def test():
-    return 'this should appear on every POST request to 0.0.0.0:5000/json'
+@app.route('/postjson', methods=["POST"])
+def normalize():
+    # get request body
+    data_request = request.get_json(force=True)
+    
+    # normalize
+    # force lowercase
+    # remove any non-letter, non-digit characters
+    new_data = {}
+    for key, value in data_request.items():
+        new_data.update({
+            key.lower()
+            .translate(str.maketrans('', '', string.punctuation)) 
+            : value,})
+
+    return jsonify(new_data)
 
 
 if __name__ == '__main__':
